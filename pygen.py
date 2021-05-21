63aa492a0129204bc2be09f389bd2e6637f48dda
lut <83720412+LinusUsernameTips@users.noreply.github.com>
2021-05-21 19:42:16 -0400

    Update pygen.py

--- pygen.py
+++ pygen.py
@@ -1,4 +1,5 @@
-import random
+import secrets
+import keyword 
 
 print("██╗     ██╗   ██╗████████╗███████╗    ██████╗  █████╗ ███████╗███████╗     ██████╗ ███████╗███╗   ██╗")
 print("██║     ██║   ██║╚══██╔══╝██╔════╝    ██╔══██╗██╔══██╗██╔════╝██╔════╝    ██╔════╝ ██╔════╝████╗  ██║")
@@ -8,34 +9,32 @@ print("███████╗╚██████╔╝   ██║   ██�
 print("╚══════╝ ╚═════╝    ╚═╝   ╚══════╝    ╚═╝     ╚═╝  ╚═╝╚══════╝╚══════╝     ╚═════╝ ╚══════╝╚═╝  ╚═══╝")                                                                                               
 stuff = ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789{[}]:;'.,/")
 while 1:
-  loopforinput = 1
-  while loopforinput <= 1.5:
+  while 1:
    try:
     pass_len = int(input("How long you want the pass / passes to be? : "))
-    loopforinput += 1
+    break 
    except ValueError:
      print("Please enter a numeric value.")
 
   multiplepassq = str(input("Do you need multiple passes? Y/N : "))
   if multiplepassq.lower() == "y":
-    loopforinput2 = 1
-    while loopforinput2 <= 1.5:
+    while 1:
      try:
       howmanypass = int(input("How many passes? : "))
-      loopforinput2 += 1
+      break
      except ValueError:
        print("Please enter a numeric value.")
     print("Ok!")
     for x in range(0, howmanypass):
      password2 = ""
      for x in range(0, pass_len):
-      grabletter = random.choice(stuff)
+      grabletter = secrets.choice(stuff)
       password2   = password2 + grabletter
      print("Password : ", password2)
   if multiplepassq.lower() == "n":
      print("Ok!")
      password = ""
      for x in range(0, pass_len):
-      grabletter = random.choice(stuff)
+      grabletter = secrets.choice(stuff)
       password  = password + grabletter
      print("Password : ", password)
